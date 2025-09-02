53cf63c4ed8e075a7a0b88aa7f14e7f289999483
kafukoM <kafukom2@gmail.com>
2025-09-02 12:30:13 +0300

    Initial commit - project structure for community skills share platform

---
+++ app.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Welcome to the community skills share platform!");
+});
+
+server.listen(3000, () => {
+  console.log("Server is listening on port 3000...");
+});
